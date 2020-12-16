b340343dcd29b62627ad27da4c1eccec41aa5a3f
claudia-ramos <cramos@fi.uba.ar>
2020-12-16 12:48:03 -0300

    mas test en PosicionTest

--- src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PosicionTest.java
@@ -15,10 +15,21 @@ public class PosicionTest {
         Posicion nuevaPosicion = posicion.desplazarPosicion(movimientoDerecha);
 
         assertEquals(6, nuevaPosicion.getHorizontal());
-
     }
+
     @Test
-    public void test02SeCambiaDePosicionHaciendoUnMovimientoHaciaArriba(){
+    public void test02SeCambiaDePosicionHaciendoUnMovimientoALaIzquierda() {
+        Posicion posicion = new Posicion(5, 5);
+
+        MovimientoIzquierda movimientoIzquierda = new MovimientoIzquierda();
+
+        Posicion nuevaPosicion = posicion.desplazarPosicion(movimientoIzquierda);
+
+        assertEquals(4, nuevaPosicion.getHorizontal());
+    }
+
+    @Test
+    public void test03SeCambiaDePosicionHaciendoUnMovimientoHaciaArriba(){
         Posicion posicion = new Posicion(5, 5);
 
         MovimientoArriba movimientoArriba = new MovimientoArriba();
@@ -27,4 +38,18 @@ public class PosicionTest {
 
         assertEquals(6, nuevaPosicion.getVertical());
     }
+
+    @Test
+    public void test04SeCambiaDePosicionHaciendoUnMovimientoHaciaAbajo(){
+        Posicion posicion = new Posicion(5, 5);
+
+        MovimientoAbajo movimientoAbajo = new MovimientoAbajo();
+
+        Posicion nuevaPosicion = posicion.desplazarPosicion(movimientoAbajo);
+
+        assertEquals(4, nuevaPosicion.getVertical());
+    }
+
+
+
 }
