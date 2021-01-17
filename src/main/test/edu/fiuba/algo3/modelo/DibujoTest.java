4e1bd1a7d6821853444ec3f2f2c738c72dc638ca
Lucas Gabriel <lgtorres@fi.uba.ar>
2021-01-17 11:35:56 -0300

    DibujoTest: modificacion prueba 05, creación prueba 06
    Prueba05: Agregado nombre descriptivo acorde a la secuencia probada.
    
    Prueba06: Se crea una secuencia de movimiento y se analiza la cantida de lineas dibujadas.

--- src/main/test/edu/fiuba/algo3/modelo/DibujoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/DibujoTest.java
@@ -86,9 +86,9 @@ public class DibujoTest {
         assertEquals( posFinalTrazoDibujado.obtenerCoordenadas(), trazoDibujado.obtenerCoordenadasPosicionFinal() );
     }
     // Este test está mal, para probar, saca los comments, y correlo, fijate que muestra solo las líneas finales del movimiento.
-       
+
     @Test
-    public void test05SeBajaElLapizYSeDibuja3Vez() {
+    public void test05SeCreaUnaSecuenciaYSeCuentaLaCantidadDeLineasDibujadasMientrasLapizEstabaApoyado() {
         lista_de_bloques.add(new BloqueBajarLapiz());
         lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
         lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
@@ -96,17 +96,70 @@ public class DibujoTest {
         lista_de_bloques.add(new BloqueMovimiento(new MovimientoArriba()));
         lista_de_bloques.add(new BloqueLevantarLapiz());
         lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
         lista_de_bloques.add(new BloqueBajarLapiz());
         lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
         lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
-
+        // Mientras el lapiz estuvo bajado, solo se hicieron 6 líneas.
 
         for (Bloque bloque : lista_de_bloques) {
             bloque.ejecutar(personaje, sectorDibujo);
         }
 
-        for (int i = 0; i < sectorDibujo.obtenerSectorDibujado().size(); i++) {
-            System.out.print("\n" + i + "| " + sectorDibujo.obtenerSectorDibujado().get(i).obtenerCoordenadasPosicionInicial() + "," + sectorDibujo.obtenerSectorDibujado().get(i).obtenerCoordenadasPosicionFinal());
+        assertEquals( 6, sectorDibujo.obtenerSectorDibujado().size() );
+    }
+
+    @Test
+    public void test06SeCreaUnaSecuenciaYSeVerificaQueLaSecuenciaEsteDibujada() {
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoArriba()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoAbajo()));
+        lista_de_bloques.add(new BloqueBajarLapiz());
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
+        lista_de_bloques.add(new BloqueMovimiento(new MovimientoDerecha()));
+        // Mientras el lapiz estuvo bajado, solo se hicieron 2 líneas.
+
+        // lineas dibujadas (3,3) a (4,-3) - (4,-3) a (5,-3)
+        List<Linea> trazosDibujados = new ArrayList<>();
+        List<List<Integer>> coordenadas_esperadas = new ArrayList<>();
+        coordenadas_esperadas.add(Arrays.asList(3, -3));
+        coordenadas_esperadas.add(Arrays.asList(4, -3));
+        coordenadas_esperadas.add(Arrays.asList(4, -3));
+        coordenadas_esperadas.add(Arrays.asList(5, -3));
+
+        for (Bloque bloque : lista_de_bloques) {
+            bloque.ejecutar(personaje, sectorDibujo);
         }
+
+        for (int k = 0; k < coordenadas_esperadas.size() - 1; k += 2) // saltando de a 2 ;
+        {
+            Linea temp_linea = new Linea(new Posicion(coordenadas_esperadas.get(k)));
+            temp_linea.setPosFinal(new Posicion(coordenadas_esperadas.get(k + 1)));
+            trazosDibujados.add(temp_linea);
+        } // generamos el array de lineas a comparar
+
+        // aquí hacemos la comparación entre las lineas en el dibujo vs las generadas para hacer la comparación.
+        for( int i = 0, j = 0 ; i < sectorDibujo.obtenerSectorDibujado().size(); i++, j += 2)
+        {
+            Posicion posInicialTrazoDibujado = new Posicion( coordenadas_esperadas.get(j)   );
+            Posicion posFinalTrazoDibujado   = new Posicion( coordenadas_esperadas.get(j+1) );
+
+            Linea trazoDibujado = sectorDibujo.obtenerSectorDibujado().get(i);
+
+            assertEquals( posInicialTrazoDibujado.obtenerCoordenadas(), trazoDibujado.obtenerCoordenadasPosicionInicial() );
+            assertEquals( posFinalTrazoDibujado.obtenerCoordenadas() , trazoDibujado.obtenerCoordenadasPosicionFinal() );
+        }
+
+        // lo dejo para hacer debug por si las dudas.
+        /*for (int i = 0; i < sectorDibujo.obtenerSectorDibujado().size(); i++) {
+            System.out.print("\n" + i + "| " + sectorDibujo.obtenerSectorDibujado().get(i).obtenerCoordenadasPosicionInicial() + "," + sectorDibujo.obtenerSectorDibujado().get(i).obtenerCoordenadasPosicionFinal());
+        }*/
     }
 }
