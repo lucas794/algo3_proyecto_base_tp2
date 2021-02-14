bf5083b5d912c3b6817da57f77a371f5c726031c
ldiazcto <ldiazc@fi.uba.ar>
2021-02-14 14:15:56 -0300

    visualizacion de division de tablero

--- src/main/java/edu/fiuba/algo3/interfaz/SectorDibujo.java
+++ src/main/java/edu/fiuba/algo3/interfaz/SectorDibujo.java
@@ -8,8 +8,8 @@ public class SectorDibujo extends GridPane{
     public SectorDibujo() {
 
         this.setPrefSize(400,500);
-        Image dibujoImagen = new Image("file:src/main/java/edu/fiuba/algo3/interfaz/imagenes/fondoSectorDibujo.jpg");
-        BackgroundImage fondoImagen = new BackgroundImage(dibujoImagen, BackgroundRepeat.REPEAT,BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        BackgroundImage fondoImagen = new BackgroundImage(new Image("file:src/main/java/edu/fiuba/algo3/interfaz/imagenes/fondoSectorDibujo.jpg"),
+                BackgroundRepeat.REPEAT,BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         Background fondo = new Background(fondoImagen);
 
         this.setBackground(fondo);
--- src/main/java/edu/fiuba/algo3/interfaz/Tablero.java
+++ src/main/java/edu/fiuba/algo3/interfaz/Tablero.java
@@ -5,19 +5,23 @@ import javafx.scene.layout.BorderPane;
 
 public class Tablero extends BorderPane {
 
+    SectorDibujo sectorDibujo = new SectorDibujo();
+    SectorBloquesDisponibles sectorBloques = new SectorBloquesDisponibles();
+    SectorAlgoritmo sectorAlgoritmo = new SectorAlgoritmo();
+
     public Tablero() {
 
-        SectorDibujo sectorDibujo = new SectorDibujo();
-        SectorBloquesDisponibles sectorBloques = new SectorBloquesDisponibles();
-        SectorAlgoritmo sectorAlgoritmo = new SectorAlgoritmo();
-
-        this.setCenter(sectorBloques);
-        this.setRight(sectorAlgoritmo);
-        this.setLeft(sectorDibujo);
+        posicionarSectores();
 
         BorderPane.setMargin(sectorDibujo, new Insets(20,50,300,30));
         BorderPane.setMargin(sectorBloques, new Insets(20,50,50,30));
         BorderPane.setMargin(sectorAlgoritmo, new Insets(20,50,50,30));
 
+    }
+
+    private void posicionarSectores() {
+        this.setCenter(sectorBloques);
+        this.setRight(sectorAlgoritmo);
+        this.setLeft(sectorDibujo);
     }
 }
