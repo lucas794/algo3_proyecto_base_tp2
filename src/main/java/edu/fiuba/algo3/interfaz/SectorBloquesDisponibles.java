4038f2f23306b70d5e12ac12d9c1483b2a7b4841
Lucas Gabriel <lgtorres@fi.uba.ar>
2021-02-15 19:55:58 -0300

    Agregado comboBox para la repetición + modificación POM
    comboBox mas bonito para la repetición de bloques.
    
    POM: Por alguna razón, no ejecuta el codecov

--- src/main/java/edu/fiuba/algo3/interfaz/SectorBloquesDisponibles.java
+++ src/main/java/edu/fiuba/algo3/interfaz/SectorBloquesDisponibles.java
@@ -1,8 +1,12 @@
 package edu.fiuba.algo3.interfaz;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class SectorBloquesDisponibles extends VBox {
@@ -17,15 +21,29 @@ public class SectorBloquesDisponibles extends VBox {
         Button botonMoverDerecha = new Button("Mover derecha");
         Button botonBajarLapiz = new Button("Bajar lapiz");
         Button botonSubirLapiz = new Button("Subir lapiz");
-        Button botonRepeticion2x = new Button("Repetir 2x");
-        Button botonRepeticion3x = new Button("Repetir 3x");
+
+        /*Button botonRepeticion2x = new Button("Repetir 2x");
+        Button botonRepeticion3x = new Button("Repetir 3x");*/
+
+        Button botonRepeticion = new Button("Repetir: ");
+        ObservableList<Integer> repeticiones = FXCollections.observableArrayList( 2, 3 );
+        ComboBox cantidadRepeticiones = new ComboBox( repeticiones );
+        cantidadRepeticiones.getSelectionModel().selectFirst();
+
         Button botonInvertir = new Button("Invertir comportamiento");
         Button botonGuardarAlgoritmo = new Button("Guardar algoritmo");
         botonGuardarAlgoritmo.setDisable( true );
 
-        VBox contenedorBotonera = new VBox(botonMoverAbajo, botonMoverArriba, botonMoverDerecha, botonMoverIzquierda,
+        HBox contenedorRepeticion = new HBox(botonRepeticion, cantidadRepeticiones);
+        contenedorRepeticion.setAlignment( Pos.CENTER );
+
+        /*VBox contenedorBotonera = new VBox(botonMoverAbajo, botonMoverArriba, botonMoverDerecha, botonMoverIzquierda,
                                            botonBajarLapiz, botonSubirLapiz, botonRepeticion2x, botonRepeticion3x,
-                                            botonInvertir, botonGuardarAlgoritmo);
+                                            botonInvertir, botonGuardarAlgoritmo);*/
+
+        VBox contenedorBotonera = new VBox(botonMoverAbajo, botonMoverArriba, botonMoverDerecha, botonMoverIzquierda,
+                botonBajarLapiz, botonSubirLapiz, contenedorRepeticion,
+                botonInvertir, botonGuardarAlgoritmo);
 
         contenedorBotonera.setAlignment( Pos.CENTER );
         contenedorBotonera.setSpacing( 20 );
