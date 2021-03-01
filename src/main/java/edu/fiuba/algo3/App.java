220eb884605e163f2cc51b7d702dd62b7d321b99
Lucas Gabriel <lgtorres@fi.uba.ar>
2021-03-01 09:04:22 -0300

    Quick fix pom + mas sonidos (Arrastre + cuando dibuja + suelta bloque)

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -7,7 +7,6 @@ import javafx.stage.Stage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import java.io.File;
-import java.time.Duration;
 
 /**
  * JavaFX App
@@ -35,9 +34,6 @@ public class App extends Application {
         stage.setTitle( TITULO_VENTANA );
         stage.setScene(scene);
         stage.show();
-
-
-
     }
 
     public static void main(String[] args) {
